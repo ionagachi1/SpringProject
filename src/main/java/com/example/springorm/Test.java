a3db5dad6db90ee8b18ac40411bc51a0880cdd43
iagachi <ion.agachi@endava.com>
2024-02-01 14:05:39 +0200

    commit14

--- src/main/java/com/example/springorm/Test.java
+++ src/main/java/com/example/springorm/Test.java
@@ -26,7 +26,7 @@ public class Test{
         List<Product> products =  productDao.findAll();
         System.out.println(products);
 
-        System.out.println("End");0
+        System.out.println("End");
 
     }
 }
